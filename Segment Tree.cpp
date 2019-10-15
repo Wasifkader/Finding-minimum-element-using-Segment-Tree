c54db3eba79c4254bef182b3513a0226e485a4c5
Wasifkader <54007718+Wasifkader@users.noreply.github.com>
2019-10-16 00:18:40 +0600

    Update Segment Tree.cpp

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -2,36 +2,20 @@
 
 using namespace std;
 
-#define LL           long long
-#define ULL          long long unsigned
-#define LD           long double
-#define ff              first
-#define ss              second
+
 #define FOR(i,n)     for(int i=0;i<n;i++)
-#define FLR(i,n)     for(LL i=0;i<n;i++)
-#define RFOR(i,n)    for(int i=n-1;i>=0;i--)
-#define RFLR(i,n)    for(LL i=n-1;i>=0;i--)
-#define rep(i,a,b)   for(int i=a;i<=b;i++)
-#define repl(i,a,b)  for(LL i=a;i<=b;i++)
-#define fast         ios_base::sync_with_stdio(0);cin.tie(0);
-#define all(a)       a.begin(),a.end()
-#define ABS(x)       ((x)<0?-(x):(x))
-#define NL           "\n"
-#define pb           push_back
-#define mk              make_pair
-#define pi           acos(-1.0)
-#define prec(n)      fixed << setprecision(n)
-#define sq(a)          (a)*(a)
 #define mx           1000000
 
 int arr[mx];
 int tree[mx*4];
 
-void build(int node,int b,int e){
-    if(b==e){
+void build(int node,int b,int e)
+{
+    if(b==e)
+    {
         tree[node]=arr[b];
         return;
-        }
+    }
     int left=node*2;
     int right=node*2+1;
     int mid=(b+e)/2;
@@ -40,9 +24,12 @@ void build(int node,int b,int e){
     tree[node]=min(tree[left],tree[right]);
 
 }
-int query(int node,int b,int e,int i,int j){
-    if(i>e||j<b)return mx;
-    else if(b>=i&&e<=j)return tree[node];
+int query(int node,int b,int e,int i,int j)
+{
+    if(i>e||j<b)
+        return mx;
+    else if(b>=i&&e<=j)
+        return tree[node];
     int left=node*2;
     int right=node*2+1;
     int mid=(b+e)/2;
@@ -54,21 +41,14 @@ int query(int node,int b,int e,int i,int j){
 
 int main()
 {
-    fast;
-    int t,i,j,k,a,b,e,q,n,kase=1,ans;
-    cin>>t;
-    while(t--){
-        cin>>n>>q;
-        FOR(i,n){
-            cin>>arr[i+1];
-        }
-        build(1,1,n);
-        //cout<<"Case "<<kase++<<":"<<endl;
-        printf("Case %d:\n",kase++);
-        FOR(i,q){
-            cin>>a>>b;
-            ans=query(1,1,n,a,b);
-            printf("%d\n",ans);
-        }
+    int t,i,j,k,a,b,e,q,n,ans;
+    cin>>n;
+       FOR(i,n)
+    {
+        cin>>arr[i+1];
     }
+    build(1,1,n);
+    cin>>a>>b;
+    ans=query(1,1,n,a,b);
+    cout<<ans<<endl;
 }
